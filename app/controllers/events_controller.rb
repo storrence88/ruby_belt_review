0a4a16595281ce9cf766b2c9d7f75eb687bcc919
Steven Torrence <steven@Stevens-MacBook-Pro.local>
2018-06-21 08:19:18 -0500

    Added out of state table and form to add event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,10 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    # @events = Event.all
+    # @host = 
+
+    @in_state_events = Event.eager_load(:user, :users_joined).where(state: current_user.state)
+		@out_state_events = Event.eager_load(:user, :users_joined).where.not(state: current_user.state)
+    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :events # Events the user has created
+  has_many :events_joined, through: :attendees, source: :event # Events the user has joined
+  
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
-  has_many :events
-  has_many :events_joined, through: :attendees, source: :event
-
+  before_save :downcase_fields
+  
   validates :first_name, :last_name, :email, :city, :state, :password, presence: true
   validates :first_name, :last_name, length: { in: 2..25 }
   validates :password, length: { minimum: 8 }
   validates :email, uniqueness: true, format: { with: EMAIL_REGEX }
 
-  before_save :downcase_fields
 
    def downcase_fields
       self.email.downcase!
